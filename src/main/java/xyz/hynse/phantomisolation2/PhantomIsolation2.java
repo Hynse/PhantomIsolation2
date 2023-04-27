0e3a1f164f79dc0b2ad98331117d3ede3c491040
MidnightTale <midnighttale24@gmail.com>
2023-04-28 06:06:48 +0700

    Initial commit

---
+++ src/main/java/xyz/hynse/phantomisolation2/PhantomIsolation2.java
@@ -0,0 +1,17 @@
+package xyz.hynse.phantomisolation2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PhantomIsolation2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
