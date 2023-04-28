397cd5940fa5d51eff1e894babcb9c3e58653499
MidnightTale <midnighttale24@gmail.com>
2023-04-28 17:05:40 +0700

    refactor commnad class

--- src/main/java/xyz/hynse/phantomisolation2/command/PhantomIsolationCommand.java
+++ src/main/java/xyz/hynse/phantomisolation2/command/PhantomIsolationCommand.java
@@ -9,45 +9,56 @@ import xyz.hynse.phantomisolation2.PhantomIsolation2;
 import xyz.hynse.phantomisolation2.util.DatabaseUtil;
 
 public class PhantomIsolationCommand implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         DatabaseUtil databaseUtil = PhantomIsolation2.databaseUtil;
+
         if (command.getName().equalsIgnoreCase("phantomisolation")) {
             if (args.length == 0) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PhantomIsolation2.phantomisolationMessageUsage));
+                sendMessage(sender, PhantomIsolation2.phantomisolationMessageUsage);
                 return true;
             }
-            if (args[0].equalsIgnoreCase("check") || args[0].equalsIgnoreCase("status")) {
-                if (sender instanceof Player) {
-                    Player player = (Player) sender;
+
+            if (!(sender instanceof Player)) {
+                sendMessage(sender, PhantomIsolation2.phantomisolationMessageNotPlayer);
+                return true;
+            }
+
+            Player player = (Player) sender;
+
+            switch (args[0].toLowerCase()) {
+                case "check":
+                case "status":
                     boolean status = databaseUtil.getPlayerIsolationStatus(player);
                     String statusText = status ? PhantomIsolation2.phantomisolationMessageStatusEnabled : PhantomIsolation2.phantomisolationMessageStatusDisabled;
                     String message = PhantomIsolation2.phantomisolationMessageStatus.replace("%status%", statusText);
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-                } else {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PhantomIsolation2.phantomisolationMessageNotPlayer));
-                }
-                return true;
-            } else if (args[0].equalsIgnoreCase("disable") || args[0].equalsIgnoreCase("off")) {
-                if (sender instanceof Player) {
-                    Player player = (Player) sender;
+                    sendMessage(sender, message);
+                    break;
+
+                case "disable":
+                case "off":
                     databaseUtil.setPlayerIsolationStatus(player, false);
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PhantomIsolation2.phantomisolationMessageDisable));
-                } else {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PhantomIsolation2.phantomisolationMessageNotPlayer));
-                }
-                return true;
-            } else if (args[0].equalsIgnoreCase("enable") || args[0].equalsIgnoreCase("on")) {
-                if (sender instanceof Player) {
-                    Player player = (Player) sender;
+                    sendMessage(sender, PhantomIsolation2.phantomisolationMessageDisable);
+                    break;
+
+                case "enable":
+                case "on":
                     databaseUtil.setPlayerIsolationStatus(player, true);
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PhantomIsolation2.phantomisolationMessageEnabled));
-                } else {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PhantomIsolation2.phantomisolationMessageNotPlayer));
-                }
-                return true;
+                    sendMessage(sender, PhantomIsolation2.phantomisolationMessageEnabled);
+                    break;
+
+                default:
+                    sendMessage(sender, PhantomIsolation2.phantomisolationMessageUsage);
             }
+            return true;
         }
         return false;
+    }
+
+    private void sendMessage(CommandSender sender, String message) {
+        if (message != null) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        }
     }
 }
