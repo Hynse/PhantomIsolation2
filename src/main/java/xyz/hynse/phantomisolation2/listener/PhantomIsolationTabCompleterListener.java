20618f7639d1b52b45fd31d9b591d5c2400bc4fb
MidnightTale <midnighttale24@gmail.com>
2023-04-28 21:17:42 +0700

    check permission before tab complete

--- src/main/java/xyz/hynse/phantomisolation2/listener/PhantomIsolationTabCompleterListener.java
+++ src/main/java/xyz/hynse/phantomisolation2/listener/PhantomIsolationTabCompleterListener.java
@@ -1,15 +1,22 @@
 package xyz.hynse.phantomisolation2.listener;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import xyz.hynse.phantomisolation2.PhantomIsolation2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PhantomIsolationTabCompleterListener implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (!sender.hasPermission("phantomisolation.use")) {
+            sendMessage(sender, PhantomIsolation2.phantomisolationMessageNoPermission);
+            return Collections.emptyList();
+        }
         if (command.getName().equalsIgnoreCase("phantomisolation") || command.getName().equalsIgnoreCase("pli")) {
             if (args.length == 1) {
                 List<String> subcommands = new ArrayList<>();
@@ -24,5 +31,10 @@ public class PhantomIsolationTabCompleterListener implements TabCompleter {
             }
         }
         return null;
+    }
+    private void sendMessage(CommandSender sender, String message) {
+        if (message != null) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        }
     }
 }
