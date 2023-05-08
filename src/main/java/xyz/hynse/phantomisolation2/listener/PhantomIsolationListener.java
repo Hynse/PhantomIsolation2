9701b64aa6c92778396ae2b30acfd74ac8478281
MidnightTale <midnighttale24@gmail.com>
2023-05-09 02:29:07 +0700

    Player in scheduler

--- src/main/java/xyz/hynse/phantomisolation2/listener/PhantomIsolationListener.java
+++ src/main/java/xyz/hynse/phantomisolation2/listener/PhantomIsolationListener.java
@@ -1,22 +1,17 @@
 package xyz.hynse.phantomisolation2.listener;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import xyz.hynse.phantomisolation2.PhantomIsolation2;
 import xyz.hynse.phantomisolation2.util.PhantomIsolationSchedulerUtil;
 
-
 public class PhantomIsolationListener implements Listener {
 
     public static void start() {
-        PhantomIsolationSchedulerUtil.runAsyncScheduler(PhantomIsolation2.instance, () -> {
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                if (PhantomIsolation2.databaseUtil.getPlayerIsolationStatus(player)) {
-                    player.setStatistic(Statistic.TIME_SINCE_REST, 0);
-                }
+        PhantomIsolationSchedulerUtil.runAsyncScheduler(PhantomIsolation2.instance, player -> {
+            if (PhantomIsolation2.databaseUtil.getPlayerIsolationStatus(player)) {
+                player.setStatistic(Statistic.TIME_SINCE_REST, 0);
             }
-        }, 1, 10); // 50 seconds = 1000 ticks
+        }, 1, 120);
     }
 }
--- src/main/java/xyz/hynse/phantomisolation2/util/PhantomIsolationSchedulerUtil.java
+++ src/main/java/xyz/hynse/phantomisolation2/util/PhantomIsolationSchedulerUtil.java
@@ -1,10 +1,12 @@
 package xyz.hynse.phantomisolation2.util;
 
-import io.papermc.paper.threadedregions.scheduler.AsyncScheduler;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
 public class PhantomIsolationSchedulerUtil {
 
     private static Boolean IS_FOLIA = null;
@@ -23,11 +25,19 @@ public class PhantomIsolationSchedulerUtil {
         return IS_FOLIA;
     }
 
-    public static void runAsyncScheduler(Plugin plugin, Runnable runnable, int initialDelayTicks, int periodTicks) {
+    public static void runAsyncScheduler(Plugin plugin, Consumer<Player> playerTask, int initialDelayTicks, int periodTicks) {
         if (isFolia()) {
-            Bukkit.getAsyncScheduler().runAtFixedRate(plugin, (task) -> runnable.run(), initialDelayTicks, periodTicks, TimeUnit.MILLISECONDS);
+            Bukkit.getAsyncScheduler().runAtFixedRate(plugin, task -> {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    playerTask.accept(player);
+                }
+            }, initialDelayTicks, periodTicks, TimeUnit.SECONDS);
         } else {
-            Bukkit.getScheduler().runTaskTimer(plugin, runnable, initialDelayTicks * 20L, periodTicks * 20L);
+            Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    playerTask.accept(player);
+                }
+            }, initialDelayTicks * 20L, periodTicks * 20L);
         }
     }
 }
